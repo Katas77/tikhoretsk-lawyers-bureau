d651182435e236eba4dc6eed7a8622ada9699587
krp77 <Krp77@mail.ru>
2025-02-24 15:37:08 +0300

    211024

--- src/main/java/com/example/tikhoretsk_lawyers_bureau_1/boards/Boards.java
+++ src/main/java/com/example/tikhoretsk_lawyers_bureau_1/boards/Boards.java
@@ -90,7 +90,7 @@ public class Boards {
         ));
 
         rowsInline.add(createButtonRow(
-                createCustomButton("КАТАСОНОВ Р.П.", "кат", "https://github.com/Katas77/tikhoretsk-lawyers-bureau-1"),
+                createCustomButton("КАТАСОНОВ Р.П.", "кат", "https://github.com/Katas77/tikhoretsk-lawyers-bureau.git"),
                 createCustomButton("КУШНИРОВА Д.А.", "куш", "https://ru.wikipedia.org/wiki/%D0%9E%D1%82%D0%BF%D1%83%D1%81%D0%BA_%D0%BF%D0%BE_%D0%B1%D0%B5%D1%80%D0%B5%D0%BC%D0%B5%D0%BD%D0%BD%D0%BE%D1%81%D1%82%D0%B8_%D0%B8_%D1%80%D0%BE%D0%B4%D0%B0%D0%BC")
         ));
 
